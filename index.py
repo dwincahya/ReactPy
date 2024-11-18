d501bcf63537a087f50f069e14b6020f648dd035
dwincahya <dwincahya8@gmail.com>
2024-11-18 23:13:09 +0700

    React Py

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 from reactpy import component, html, run, hooks
 
 @component
-def Gallery(width, height):
-    is_show, set_is_show = hooks.use_state(false)
+def Gallery(is_show, width, height):
+    is_show, set_is_show = hooks.use_state(False)
     
     if is_show:
         return html.img(
@@ -16,14 +16,21 @@ def Gallery(width, height):
     
 
 @component
-def Button():
-    def show_image_handler():
+def Button(is_show, set_is_show):
+    
+    def show_image_handler(event):
+        set_is_show(not is_show)
+        
+    return html.button({"onclick": show_image_handler}, "Munculkan Gambar" )
 
 @component
 def App():
+    is_show, set_is_show = hooks.use_state(False)
+    
     return html._(
         html.h1("Testing REACTPY"),
-        Gallery(width=720, height=480),
+        Button(is_show, set_is_show),
+        Gallery(is_show, width=720, height=480),
     )
 
 
